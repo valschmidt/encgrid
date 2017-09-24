46b464b54dd79f0555c93a7acfc8979c17acd1fb
Val E. Schmidt <vschmidt@ccom.unh.edu>
2017-09-24 15:21:38 -0700

    Initial Commit

---
+++ encgrid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  encgrid
+//
+//  Created by Val E. Schmidt on 9/24/17.
+//  Copyright © 2017 Val E. Schmidt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
